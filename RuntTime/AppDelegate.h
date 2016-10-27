551e39093d98b80010ee8b8438e765b9e096c772
hyc <hyc@hyc.local>
2016-10-27 16:53:05 +0800

    Initial Commit

---
+++ RuntTime/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RuntTime
+//
+//  Created by hyc on 16/10/27.
+//  Copyright © 2016年 baohe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
